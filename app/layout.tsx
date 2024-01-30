51b009b771fe72d6309af303c23e1476005ddae0
Juan Escalada <juanescalada175@gmail.com>
2024-01-30 14:06:01 -0800

    Fix bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <div className="fixed h-screen w-full bg-gradient-to-br from-[#ffdddd] via-white to-[#ddeeff]" />
         <Suspense fallback="...">
           <Nav />
         </Suspense>
