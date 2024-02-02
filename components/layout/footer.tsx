deea41624c98dcbc4c3905314c9c85f755c312a1
Juan Escalada <juanescalada175@gmail.com>
2024-02-01 16:37:53 -0800

    Improve footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         A project by{" "}
         <a
           className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://twitter.com/steventey"
+          href="https://github.com/jescalada"
           target="_blank"
           rel="noopener noreferrer"
         >
