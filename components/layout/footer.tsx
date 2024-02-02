ffde535e736b2439de73d9dfc38f1ffe3e7d2525
Juan Escalada <juanescalada175@gmail.com>
2024-02-01 16:37:11 -0800

    Remove coffee thingy

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -14,13 +14,6 @@ export default function Footer() {
           BCIT Students
         </a>
       </p>
-      <a
-        href="https://www.buymeacoffee.com/steventey"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mx-auto mt-2 flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-6 py-2 transition-all duration-75 hover:scale-105"
-      >
-      </a>
     </div>
   );
 }
