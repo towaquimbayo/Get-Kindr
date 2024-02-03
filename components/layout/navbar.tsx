1abca9f8bc44c970665c0d4b76b020fd071d617f
Towa Quimbayo <towacurtis@hotmail.com>
2024-02-02 16:33:55 -0800

    updated desktop view navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -3,48 +3,54 @@
 import Image from "next/image";
 import Link from "next/link";
 import useScroll from "@/lib/hooks/use-scroll";
-import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
 
 export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
 
   return (
-    <>
-      <SignInModal />
-      <div
-        className={`fixed top-0 w-full flex justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
-      >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" className="flex items-center font-display text-2xl">
-            <Image
-              src="/kindr_logo.png"
-              alt="KINDR logo"
-              width="96"
-              height="96"
-              className="mr-2 rounded-sm"
-            ></Image>
+    <div
+      className={`fixed top-0 flex w-full justify-center ${
+        scrolled ? "bg-white/50 shadow-md backdrop-blur-xl" : "bg-white/0"
+      } z-30 transition-all`}
+    >
+      <div className="mx-5 my-1 flex min-h-16 w-full max-w-screen-xl items-center justify-between">
+        <Link href="/" className="flex items-center font-display text-2xl">
+          <Image
+            src="/kindr_logo.png"
+            alt="KINDR logo"
+            width="100"
+            height="100"
+            objectFit="contain"
+            className="mr-2"
+          />
+        </Link>
+        <div className="flex items-center space-x-8">
+          <Link
+            href="/about"
+            className="transition-all ease-in-out hover:text-primary"
+          >
+            About
           </Link>
-          <div>
-            {session ? (
-              <UserDropdown session={session} />
-            ) : (
-              <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-base font-semibold text-white transition-all hover:bg-white hover:text-black"
-                onClick={() => setShowSignInModal(true)}
-              >
-                Sign In
-              </button>
-            )}
-          </div>
+          <Link
+            href="/events"
+            className="transition-all ease-in-out hover:text-primary"
+          >
+            Events
+          </Link>
+          {session ? (
+            <UserDropdown session={session} />
+          ) : (
+            <Link
+              href="/login"
+              className="transition-all ease-in-out hover:text-primary"
+            >
+              Login
+            </Link>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 }
