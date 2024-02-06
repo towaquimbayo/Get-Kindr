87a9ddcdc1a5ed48f5e0a3a148f019df64886a18
Juan Escalada <juanescalada175@gmail.com>
2024-02-06 15:14:19 -0800

    Add basic database call to front page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,8 @@ import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
 import { nFormatter } from "@/lib/utils";
 
+import prisma from "@/lib/prisma";
+
 export default async function Home() {
   const { stargazers_count: stars } = await fetch(
     "https://api.github.com/repos/steven-tey/precedent",
@@ -22,21 +24,26 @@ export default async function Home() {
   )
     .then((res) => res.json())
     .catch((e) => console.log(e));
+    
+  const events = await prisma.event.findMany();
 
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        {/* <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-[#ffdddd] px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing KINDR
-          </p>
-        </a> */}
+        <div className="border-pink-300 border-2 rounded-xl p-4">
+          <h2 className="text-xl font-semibold mb-2">Events</h2>
+          {events.map((event) => (
+            <div className="mb-2" key={event.id}>
+              <h1 className="text-lg">Name: {event.name}</h1>
+              <p>Description: {event.description}</p>
+              <p>Address: {event.address}</p>
+              <p>City: {event.city}</p>
+              <p>Start Time: {event.start_time.toString()}</p>
+              <p>End Time: {event.end_time.toString()}</p>
+            </div>
+          ))}
+        </div>
+        
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
