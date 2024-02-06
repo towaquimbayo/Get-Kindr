93afbf8d2a369872ee7c176a68c09a60d6c667d2
Juan Escalada <juanescalada175@gmail.com>
2024-02-06 15:14:43 -0800

    Add create-events-table endpoint

---
+++ app/api/create-events-table/route.ts
@@ -0,0 +1,34 @@
+import prisma from "@/lib/prisma";
+
+/*
+ * This function will create a table in the database if it does not exist.
+ * @param {Request} request - The incoming request
+ * @returns {Response} - The response to the incoming request
+ */
+export async function GET(request: Request) {
+  try {
+    await prisma.$executeRaw`
+      CREATE TABLE IF NOT EXISTS public."Event" (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        name TEXT NOT NULL,
+        description TEXT,
+        start_time TIMESTAMP NOT NULL,
+        end_time TIMESTAMP NOT NULL,
+        organization_id UUID NOT NULL,
+        tags TEXT[],
+        address TEXT NOT NULL,
+        city TEXT NOT NULL,
+        recurring BOOLEAN NOT NULL,
+        online BOOLEAN NOT NULL,
+        token_bounty INT NOT NULL,
+        number_of_spots INT NOT NULL,
+        event_volunteers UUID[]
+        -- organizationId UUID REFERENCES Organization(id) Todo: MISSING - Add later
+      )`;
+  } catch (error) {
+    return new Response("Error creating events table: " + error, {
+      status: 500,
+    });
+  }
+  return new Response("Events table created", { status: 200 });
+}
