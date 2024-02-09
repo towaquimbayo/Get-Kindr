36eb85358727d327a3cd8ea91adf33866d6f1537
Braden R <brogers42@my.bcit.ca>
2024-02-09 12:38:09 -0800

    Front end of recovery page version 1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default async function RootLayout({
   return (
     <>
       <Nav />
-      <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+      <main className="flex min-h-screen w-full flex-col items-center justify-center py-8 md:pt-24 pb-12">
         {children}
       </main>
       <Footer />
