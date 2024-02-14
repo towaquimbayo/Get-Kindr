a5ddcc0894c7a6fd872618b699f0b91af4abdf57
Juan Escalada <juanescalada175@gmail.com>
2024-02-13 18:35:25 -0800

    Add basic signup flow for email/password

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { hash } from "bcrypt";
+import prisma from "@/lib/prisma";
+import { AccountType } from "@prisma/client";
 
 export async function POST(req: Request) {
   try {
@@ -12,18 +14,56 @@ export async function POST(req: Request) {
       organization_name,
     } = await req.json();
 
-    // TODO: Check if email already exists
-    // TODO: Save user to database
-    // TODO: Send verification email
-    console.log("Signup Data: ", {
-      first_name,
-      last_name,
-      email,
-      is_organization,
-      organization_name,
+    // Check if user already exists
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
 
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        { user: null, message: "User already exists" },
+        { status: 400 },
+      );
+    }
+
     const hashedPassword = await hash(password, 16);
+
+    // Create user in database
+    const user = await prisma.user.create({
+      data: {
+        name: first_name + " " + last_name,
+        email,
+        hashedPassword,
+        image:
+          "https://ui-avatars.com/api/?name=" + first_name + "+" + last_name,
+        accountType: is_organization
+          ? AccountType.ORGANIZATION
+          : AccountType.VOLUNTEER,
+        accounts: {},
+        organization: is_organization
+          ? {
+              create: {
+                name: organization_name,
+                description: "",
+                events: {},
+              },
+            }
+          : {},
+        volunteer: is_organization
+          ? {}
+          : {
+              create: {
+                bio: "This is my bio",
+                phoneNumber: "123-456-7890",
+                admin: false,
+              },
+            },
+      },
+    });
+
+    console.log("User created: ", user);
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (e) {
