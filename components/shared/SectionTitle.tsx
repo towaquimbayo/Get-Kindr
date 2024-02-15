ac1242e766ab97fe1e51f8a4b042f8fc5d25fb29
noufilsaqib <noufil.s@hotmail.com>
2024-02-15 14:30:57 -0800

    Created helper components for layout

---
+++ components/shared/SectionTitle.tsx
@@ -0,0 +1,34 @@
+import Container from "../layout/Container";
+
+const SectionTitle = ({ children, align, pretitle, title }: {
+    children?: React.ReactNode;
+    align?: string;
+    pretitle?: string;
+    title: string;
+}) => {
+  return (
+    <Container className={`flex w-full flex-col mt-6 ${
+        align === "left" ? "" : "items-center justify-center text-center"
+    }`}>
+        {pretitle && (
+            <div className="text-md font-bold tracking-widest text-teal-500 uppercase">
+                {pretitle}
+            </div>
+        )}
+
+        {title && (
+            <h2 className="max-w-lg mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-5xl">
+                {title}
+            </h2>
+        )}
+
+        {children && (
+            <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl">
+                {children}
+            </p>
+        )}
+    </Container>
+  );
+}
+
+export default SectionTitle;
