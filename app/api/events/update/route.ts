242465858fa62bc032827a8a7e725a1d917f9246
Juan Escalada <juanescalada175@gmail.com>
2024-02-16 10:03:19 -0800

    Add PUT endpoint for updating events

---
+++ app/api/events/update/route.ts
@@ -0,0 +1,72 @@
+import prisma from "@/lib/prisma";
+
+/**
+ * Endpoint to update an event by passing in the event object
+ * @endpoint PUT /events/update
+ * @param {Request} request - The incoming request
+ * @returns {Response} - The response to the incoming request
+ */
+export async function PUT(request: Request) {
+  try {
+    const {
+      id,
+      name,
+      description,
+      start_time,
+      end_time,
+      organization_id,
+      tags,
+      address,
+      city,
+      recurring,
+      online,
+      token_bounty,
+      number_of_spots,
+    } = await request.json();
+
+    if (!id) {
+      return new Response("Missing id", {
+        status: 400,
+      });
+    }
+
+    if (
+      !name ||
+      !start_time ||
+      !end_time ||
+      !organization_id ||
+      !address ||
+      !city ||
+      !token_bounty ||
+      !number_of_spots
+    ) {
+      return new Response("Missing required fields", {
+        status: 400,
+      });
+    }
+
+    const updatedEvent = await prisma.event.update({
+      where: { id },
+      data: {
+        name,
+        description,
+        start_time: new Date(start_time),
+        end_time: new Date(end_time),
+        organization_id,
+        tags,
+        address,
+        city,
+        recurring,
+        online,
+        token_bounty,
+        number_of_spots,
+      },
+    });
+    return new Response(JSON.stringify(updatedEvent), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response("Error updating event: " + error, {
+      status: 500,
+    });
+  }
+}
