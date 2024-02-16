8fe236a799ccca9f859b5f6342bbd5987ef4a775
Juan Escalada <juanescalada175@gmail.com>
2024-02-16 09:30:58 -0800

    Fix deploy bug

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -78,9 +78,9 @@ export async function POST(req: Request) {
               create: {
                 bio: "This is my bio",
                 phoneNumber: "123-456-7890",
-                admin: false,
               },
             },
+        admin: false,
       },
     });
 
