5187c0e3b70dda8b174d88a656926362a4256907
Braden R <brogers42@my.bcit.ca>
2024-03-08 13:04:51 -0800

    Enabled server actions needed for password check.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com", "ui-avatars.com", "api.dicebear.com"],
   },
