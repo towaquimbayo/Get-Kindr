520a876a248c7370b391d90558b2eaf0b395a9ab
Braden R <brogers42@my.bcit.ca>
2024-03-09 13:24:04 -0800

    Setup reset password with OTP confirmation.

--- app/(root)/reset-password/page.tsx
+++ app/(root)/reset-password/page.tsx
@@ -63,7 +63,7 @@ export default function Recovery() {
       setRepPassColor('red');
     }
   }
-  
+
   const [viewPassIcon, setViewPassIcon] = useState<string>('M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z');
   const [viewPassColor, setViewPassColor] = useState<string>('black');
   const [showPassType, setShowPassType] = useState<string>('password');
@@ -100,12 +100,38 @@ export default function Recovery() {
     }
   }
 
-  const resetPassword = () => {
+  const [OTP, setOTP] = useState<string>('password');
+  const resetPassword = async () => {
     const pass = document.getElementsByTagName('input')[0].value;
     const repPass = document.getElementsByTagName('input')[1].value;
+    let responseText = "Verifying One Time Passcode..."
     if (pass == repPass) {
       if (checkPassword(pass)) {
-        console.log('Password Reset');
+        const response = document.getElementById('response');
+        if (response) {
+          response.classList.remove('opacity-0');
+          response.classList.remove('h-0');
+          response.classList.remove('mt-0');
+          response.classList.remove('mb-0');
+          response.innerHTML = responseText;
+        }
+        const res = await fetch('/api/one-time-pass/verify/?otp=' + OTP, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (res.ok) {
+          if (response) {
+            response.innerHTML = "Password Reset.";
+            response.classList.add('text-tertiary');
+          }
+        } else {
+          if (response) {
+            response.innerHTML = "Invalid One Time Passcode.";
+            response.classList.add('text-primary');
+          }
+        }
       }
     }
   }
@@ -114,7 +140,11 @@ export default function Recovery() {
     <div className="flex flex-1 flex-col items-center place-content-center w-full h-4/5 bg-gradient-to-r from-start to-end">
       <div className="flex flex-col items-center md:w-2/3 w-11/12 h-fit mb-20 md:px-0 px-6 md:mb-8 border-4 rounded-lg border-secondary bg-gray-50 mt-24">
         <p className="md:mb-12 mb-16 mt-6 text-center font-display font-bold text-4xl md:text-5xl">Password Reset?</p>
-        <p className="mb-4 pl-4 sm:pl-10 font-display text-xl sm:text-2xl font-bold  text-secondary md:w-2/3 w-full ">Enter your new password.</p>
+        <p className="mb-4 pl-4 sm:pl-10 font-display text-xl sm:text-2xl font-bold  text-secondary md:w-2/3 w-full ">Enter your One Time Passcode.</p>
+        <div className="md:w-2/3 w-full flex mb-8 md:pr-14 pr-20">
+          <input className="md:h-12 h-16 px-6 md:mt-0 mt-2 rounded-lg border border-[#EAEAEA] w-full" onChange={(e) => setOTP(e.target.value)} placeholder=""></input>
+        </div>
+        <p className="mb-4 pl-4 sm:pl-10 font-display text-xl sm:text-2xl font-bold text-secondary md:w-2/3 w-full ">Enter your new password.</p>
         <div className="md:w-2/3 w-full flex">
           <input className="md:h-12 h-16 px-6 md:mt-0 mt-2 rounded-lg border border-[#EAEAEA] w-full" type={showPassType} onBlur={(e) => checkUpdatePassword(e.target.value)} onChange={(e) => setPassword(e.target.value)} placeholder=""></input>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke={passColor} className="ml-4 md:w-12 md:h-12 w-20 h-20">
@@ -148,7 +178,7 @@ export default function Recovery() {
           <Link href="/" className="w-1/3"><button className="w-full text-md h-12 rounded-md bg-secondary bg-opacity-60 text-white focus:outline-none hover:opacity-80 transition-all duration-300">Cancel</button></Link>
           <button id="submit" className="text-md h-12 w-1/3 text-md h-12 w-1/3 rounded-md focus:outline-none border-primary transition-all duration-300 block px-4 bg-primary text-white hover:opacity-80 !bg-[#E5E5E5] text-[#BDBDBD] cursor-not-allowed" onClick={resetPassword}>Reset</button>
         </div>
-        <p className="text-center font-display text-lg font-bold text-primary opacity-0">Password Reset.</p>
+        <p id="response" className="text-center font-display text-2xl font-bold text-secondary mt-6 mb-6 opacity-0 h-0 mb-0 mt-0">Password Reset.</p>
       </div>
     </div>
   );
