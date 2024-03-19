9385b5f8d4d22793db2002ca8a9c8b935e4145d6
Juan Escalada <juanescalada175@gmail.com>
2024-03-19 12:39:29 -0700

    Fix OTP deployment issue

--- app/api/one-time-pass/create/route.ts
+++ app/api/one-time-pass/create/route.ts
@@ -7,63 +7,55 @@ import prisma from "@/lib/prisma";
  * @endpoint POST /api/one-time-pass/create
  */
 export async function POST(request: Request) {
-    let one_time_pass;
+  let one_time_pass;
 
-    try {
-        // Get all the new OTP info from the request
-        const {
-            email,
-            expiration_date,
-        } = await request.json();
+  try {
+    // Get all the new OTP info from the request
+    const { email, expiration_date } = await request.json();
 
-        // If any of the required fields are missing, return an error
-        if (
-            !email ||
-            !expiration_date
-        ) {
-            return new Response("Missing required fields", {
-                status: 400,
-            });
-        }
-
-        let unique = false;
-        
-
-        while (!unique) {
-            one_time_pass = Math.random().toString(36).slice(2, 10);
-            const existingOTP = await prisma.OneTimePass.findFirst({
-                where: {
-                    OneTimePass: one_time_pass,
-                },
-            });
-            unique = !existingOTP;
-        }
-
-        const newOTP = {
-            userEmail: email,
-            OneTimePass: one_time_pass,
-            expires: new Date(new Date().getTime() + 15 * 60 * 1000).toISOString(),
-        };
-
-        // Add the new OTP to the database
-        await prisma.OneTimePass.create({
-            data: newOTP,
-        });
-
-    } catch (error) {
-        console.error(error);
-        return new Response("Error creating OTP: " + error, {
-            status: 500,
-        });
+    // If any of the required fields are missing, return an error
+    if (!email || !expiration_date) {
+      return new Response("Missing required fields", {
+        status: 400,
+      });
     }
 
-    const response = {
-        "one_time_pass": one_time_pass,
-        "success": true
+    let unique = false;
+
+    while (!unique) {
+      one_time_pass = Math.random().toString(36).slice(2, 10);
+      const existingOTP = await prisma.oneTimePass.findFirst({
+        where: {
+          OneTimePass: one_time_pass,
+        },
+      });
+      unique = !existingOTP;
+    }
+
+    const newOTP = {
+      userEmail: email,
+      OneTimePass: one_time_pass,
+      expires: new Date(new Date().getTime() + 15 * 60 * 1000).toISOString(),
     };
 
-    // Return the created OTP
-    return new Response(JSON.stringify(response), {
-        status: 200,
+    // Add the new OTP to the database
+    await prisma.OneTimePass.create({
+      data: newOTP,
     });
+  } catch (error) {
+    console.error(error);
+    return new Response("Error creating OTP: " + error, {
+      status: 500,
+    });
+  }
+
+  const response = {
+    one_time_pass: one_time_pass,
+    success: true,
+  };
+
+  // Return the created OTP
+  return new Response(JSON.stringify(response), {
+    status: 200,
+  });
 }
--- app/api/one-time-pass/delete/route.ts
+++ app/api/one-time-pass/delete/route.ts
@@ -7,22 +7,22 @@ import prisma from "@/lib/prisma";
  * @returns {Response} - The response to the incoming request
  */
 export async function DELETE(request: Request) {
-    try {
-        const otp = await request.json();
-        const OTP = otp.OneTimePass;
-        if (!OTP) {
-            return new Response("Missing OTP", {
-                status: 400,
-            });
-        }
-        await prisma.OneTimePass.delete({
-            where: { OneTimePass: OTP },
-        });
-        return new Response("OTP deleted", { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return new Response("Error deleting OTP: " + error, {
-            status: 500,
-        });
+  try {
+    const otp = await request.json();
+    const OTP = otp.OneTimePass;
+    if (!OTP) {
+      return new Response("Missing OTP", {
+        status: 400,
+      });
     }
+    await prisma.oneTimePass.delete({
+      where: { OneTimePass: OTP },
+    });
+    return new Response("OTP deleted", { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response("Error deleting OTP: " + error, {
+      status: 500,
+    });
+  }
 }
