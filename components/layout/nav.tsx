b01d66df278a83c2bd62f6589b9ac3e89d7e2add
Towa Quimbayo <towacurtis@hotmail.com>
2024-03-26 12:26:33 -0700

    user login fixed

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,8 +1,9 @@
-import Navbar from "./navbar";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+"use client";
 
-export default async function Nav() {
-  const session = await getServerSession(authOptions);
+import Navbar from "./navbar";
+import { useSession } from "next-auth/react";
+
+export default function Nav() {
+  const { data: session } = useSession();
   return <Navbar session={session} />;
 }
