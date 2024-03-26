58009bcaa92ae236c5f0063f10fa0baef815e2e6
Towa Quimbayo <towacurtis@hotmail.com>
2024-03-25 21:46:55 -0700

    fetched events from db for my events

--- app/(root)/my-events/page.tsx
+++ app/(root)/my-events/page.tsx
@@ -17,6 +17,7 @@ const EventCard = React.memo(function EventCard({
 
   function getDate(date: string) {
     return new Date(date).toLocaleDateString("en-US", {
+      timeZone: "UTC",
       month: "long",
       day: "numeric",
       year: "numeric",
@@ -30,11 +31,14 @@ const EventCard = React.memo(function EventCard({
     });
   }
 
+  // Title case the status
+  const statusName =
+    event.status.charAt(0).toUpperCase() + event.status.slice(1);
   return (
     <Card
       key={event.id}
       shadow="none"
-      className="rounded-lg border border-[#EAEAEA] bg-white !transition-all duration-300 ease-in-out hover:shadow-md"
+      className="w-full rounded-lg border border-[#EAEAEA] bg-white !transition-all duration-300 ease-in-out hover:shadow-md"
     >
       <CardBody
         className="w-full p-6 hover:cursor-pointer md:p-8"
@@ -49,7 +53,7 @@ const EventCard = React.memo(function EventCard({
             {isOrganization && <Edit size={18} />}
           </div>
           <div className="rounded-full bg-primary px-4 py-2 text-white">
-            <p className="px-1">{event.status}</p>
+            <p className="px-1">{statusName}</p>
           </div>
         </div>
         <div className="mb-4 flex w-full flex-col items-start gap-2 text-sm md:flex-row md:items-center lg:gap-4 lg:text-base">
@@ -89,82 +93,82 @@ export default function MyEvents() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  const tabLabels = ["All", "Completed", "Upcoming"];
   const [selectedTab, setSelectedTab] = useState("All");
-  const userTokens = 0;
+  const [events, setEvents] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [totalVolHours, setTotalVolHours] = useState(0);
+
+  const tabLabels = ["All", "Completed", "Upcoming"];
   const isOrganization = session?.accountType.toLowerCase() === "organization";
 
   useEffect(() => {
     if (!session || status !== "authenticated") router.push("/login");
   }, [session, status, router]);
 
-  const filteredEvents = useMemo(() => {
-    const events = [
-      {
-        id: 1,
-        name: "School Outreach Activity Leader",
-        description:
-          "Every year, we bring arts and culture to classrooms across Vancouver. Through craft workshops and storytelling, we learn about the diverse cultures that make up our wonderful community. If you like stories, and being creative, volunteer to be a ...",
-        startTime: "2022-10-01T09:00:00",
-        endTime: "2022-10-01T12:00:00",
-        organization: "Vancouver School Board",
-        tags: ["Education", "Arts", "Children", "Inperson", "Storytelling"],
-        location: "Vancouver, BC",
-        status: "Completed",
-        isFavourite: false,
-      },
-      {
-        id: 2,
-        name: "Community Garden Maintenance",
-        description:
-          "We are looking for volunteers to help us maintain our community garden. We have a variety of tasks available, including weeding, watering, and planting. If you have a green thumb, or are looking to develop one, we would love to have you join us!",
-        startTime: "2022-10-21T09:00:00",
-        endTime: "2022-10-21T12:00:00",
-        organization: "Burnaby Community Garden Society",
-        tags: ["Gardening", "Community", "Outdoors", "Inperson"],
-        location: "Burnaby, BC",
-        status: "Upcoming",
-        isFavourite: true,
-      },
-      {
-        id: 3,
-        name: "Food Bank Volunteer",
-        description:
-          "We are looking for volunteers to help us sort and pack food donations. We are also looking for volunteers to help us distribute food to our clients. If you are looking to make a difference in your community, we would love to have you join us!",
-        startTime: "2022-10-01T09:00:00",
-        endTime: "2022-10-01T12:00:00",
-        organization: "Richmond Food Bank",
-        tags: ["Community", "Food", "Inperson"],
-        location: "Richmond, BC",
-        status: "Completed",
-        isFavourite: true,
-      },
-      {
-        id: 4,
-        name: "Community Kitchen Volunteer",
-        description:
-          "We are looking for volunteers to help us prepare and serve meals at our community kitchen. If you are passionate about food, and want to make a difference in your community, we would love to have you join us!",
-        startTime: "2022-10-01T09:00:00",
-        endTime: "2022-10-01T12:00:00",
-        organization: "North Vancouver Community Kitchen",
-        tags: ["Community", "Food", "Inperson"],
-        location: "North Vancouver, BC",
-        status: "Upcoming",
-        isFavourite: false,
-      },
-    ];
+  useEffect(() => {
+    async function fetchEvents() {
+      await fetch("/api/events", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(async (res) => {
+          const events = await res.json();
+          if (!events || !events.length || events.length === 0) {
+            console.error("No events found", events);
+            return;
+          }
+          console.log("Fetched Events:", events);
+          
+          let totalHours = 0;
+          const formattedEvents = events.map((event: any) => {
+            // Calculate total hours
+            const startTime = new Date(event.start_time);
+            const endTime = new Date(event.end_time);
+            totalHours += (endTime.getTime() - startTime.getTime()) / (1000 * 60 * 60);
 
-    return events.filter((event) => {
+            // Format the event data
+            return {
+              id: event.id,
+              name: event.name,
+              description: event.description,
+              startTime: event.start_time,
+              endTime: event.end_time,
+              organization: event.organization.name,
+              tags: event.tags,
+              location: event.city + ", " + event.province,
+              status: event.status.toLowerCase(),
+              isFavourite: false,
+            };
+          });
+          console.log("Formatted Events:", formattedEvents);
+          setEvents(formattedEvents || []);
+          setTotalVolHours(totalHours);
+        })
+        .catch((error) => {
+          console.error("Error fetching events: ", error);
+        })
+        .finally(() => {
+          setIsFetching(false);
+        });
+    }
+    fetchEvents();
+  }, []);
+
+  const filteredEvents = useMemo(() => {
+    if (!events || !events.length || events.length === 0) return [];
+    return events.filter((event: { status: string }) => {
       if (selectedTab === "All") return true;
       if (selectedTab === "Completed") {
-        return event.status === "Completed";
+        return event.status === "completed";
       }
       if (selectedTab === "Upcoming") {
-        return event.status === "Upcoming";
+        return event.status === "upcoming";
       }
       return false;
     });
-  }, [selectedTab]);
+  }, [events, selectedTab]);
 
   return (
     <div className="mx-auto mb-auto mt-28 flex w-full max-w-screen-xl flex-col gap-8 p-8">
@@ -173,36 +177,46 @@ export default function MyEvents() {
           My Events
         </h2>
         <p className="text-lg">
-          Total hours: <span className="font-bold">{userTokens}h</span>
+          Total hours: <span className="font-bold">{totalVolHours}h</span>
         </p>
       </div>
 
-      <div className="flex w-full flex-col">
-        <Tabs
-          color="primary"
-          aria-label="Tab labels"
-          radius="lg"
-          size="lg"
-          className="mb-8"
-          onSelectionChange={(key: Key) => {
-            setSelectedTab(tabLabels[tabLabels.indexOf(key as string)]);
-          }}
-        >
-          {tabLabels.map((label) => (
-            <Tab key={label} title={<div className="px-2">{label}</div>}>
-              <div className="flex flex-wrap gap-8">
-                {filteredEvents.map((event) => (
-                  <EventCard
-                    key={event.id}
-                    event={event}
-                    isOrganization={isOrganization}
-                  />
-                ))}
-              </div>
-            </Tab>
-          ))}
-        </Tabs>
-      </div>
+      {isFetching ? (
+        <p className="animate-pulse text-[#858585] transition-all">
+          Loading events...
+        </p>
+      ) : (
+        <div className="flex w-full flex-col">
+          <Tabs
+            color="primary"
+            aria-label="Tab labels"
+            radius="lg"
+            size="lg"
+            className="mb-8"
+            onSelectionChange={(key: Key) => {
+              setSelectedTab(tabLabels[tabLabels.indexOf(key as string)]);
+            }}
+          >
+            {tabLabels.map((label) => (
+              <Tab key={label} title={<div className="px-2">{label}</div>}>
+                <div className="flex flex-wrap gap-8">
+                  {filteredEvents.length === 0 ? (
+                    <p>No events found</p>
+                  ) : (
+                    filteredEvents.map((event: { id: string }) => (
+                      <EventCard
+                        key={event.id}
+                        event={event}
+                        isOrganization={isOrganization}
+                      />
+                    ))
+                  )}
+                </div>
+              </Tab>
+            ))}
+          </Tabs>
+        </div>
+      )}
     </div>
   );
 }
