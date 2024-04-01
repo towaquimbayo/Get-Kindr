4c0c8c936005c359d659891ade54e303c7e418ae
Braden R <brogers42@my.bcit.ca>
2024-04-01 15:40:04 -0700

    Documented API endpoints for finding users.

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -1,28 +1,32 @@
 import prisma from "@/lib/prisma";
 
 /**
- * Endpoint for finding all accounts with matching emails: GET /api/emails
+ * Endpoint for finding all accounts with matching emails: GET /api/emails?email=
  * Param: email - The email to check against
  * @param {Request} request - The incoming request
  */
 export async function GET(request: Request) {
     try {
 
-        // get the data from the request ?email=...
+        // Get the email from the request.
         const email = request.url.split("=")?.[1];
         const decodedEmail = decodeURIComponent(email);
 
+        // Find the user with the matching email.
         const result = await prisma.user.findFirst({
             where: { email: decodedEmail }
         })
 
         if (result) {
+            // If the user exists, return true.
             return new Response(JSON.stringify(true), { status: 200 });
         } else {
+            // If the user does not exist, return false.
             return new Response(JSON.stringify(false), { status: 200 });
         }
 
     } catch (error) {
+        // If an error occurs return an error response.
         console.error(error);
         return new Response("Error searching for user emails: " + error, {
             status: 500,
--- app/api/organizations/find-by-user/route.ts
+++ app/api/organizations/find-by-user/route.ts
@@ -1,13 +1,15 @@
 import prisma from "@/lib/prisma";
 
 /**
- * Endpoint for getting all organizations: GET /api/organizations/find-by-user
+ * Endpoint for getting all organizations: GET /api/organizations/find-by-user?organization_id=
  * Param: organization_id - The organization id
  * @param {Request} request - The incoming request
  */
 export async function GET(request: Request) {
   try {
+    // Get the organization_id from the request.
     const userID = request.url.split("=")?.[1];
+    // If organization_id is present, get info for that organization
     const organizations = userID
       ? await prisma.organization.findFirst({
         where: { userId: userID },
@@ -16,9 +18,10 @@ export async function GET(request: Request) {
           },
         })
       : await prisma.organization.findMany({ include: { user: true } });
-
+    // Return the organizations found.
     return new Response(JSON.stringify(organizations), { status: 200 });
   } catch (error) {
+    // If an error occurs return an error response.
     console.error(error);
     return new Response("Error getting organizations: " + error, {
       status: 500,
